6fb73e60c1f51e57dd75048e0c5f1444507c6f60
zhiyuan_zhu <zhiyuan_zhu@htc.com>
2017-09-08 21:41:52 +0800

    add doc

--- memory/mem_main.c
+++ memory/mem_main.c
@@ -1,12 +1,130 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/delay.h>
+#include <linux/sched.h>
+#include <linux/mm_types.h>
 
 MODULE_LICENSE("GPL");
 
 extern char *get_buf(void);
 extern int mem_proc_init(void);
 extern int mem_proc_exit(void);
+
+static long ft2_get_pid(char *buf) {
+	char *tmp = NULL;
+	long pid = -1;
+
+	if (NULL == buf) {
+		printk(KERN_ALERT "buf==NULL\n");
+		goto get_pid_fail;
+	}
+
+	tmp = strstr(buf, "pid");
+	if(NULL == tmp) {
+		printk(KERN_ALERT "tmp==NULL, buf=%s\n", buf);
+		goto get_pid_fail;
+	}
+
+	tmp += 4;
+	while(*tmp == ' ') {
+		tmp++;
+	}
+
+	buf = tmp;
+	tmp = strchr(buf, ' ');
+	if(tmp) {
+		*tmp = '\0';
+	}
+
+	if(kstrtol(buf, 10, &pid) == 0) {
+		*tmp = ' ';
+		return pid;
+	}
+
+	printk(KERN_ALERT "kstrtol fail, tmp: %s\n", tmp);
+
+get_pid_fail:
+	printk(KERN_ALERT "get pid fail\n");
+	return -1;
+}
+
+static unsigned long long ft2_get_malloc_addr(char *buf) {
+	char *tmp = NULL;
+	unsigned long long addr;
+
+	if(NULL == buf) {
+		printk(KERN_ALERT "buf==NULL\n");
+		goto get_malloc_addr_fail;
+	}
+
+	tmp = strstr(buf, "malloc_addr:");
+	if(NULL == tmp) {
+		printk(KERN_ALERT "tmp==NULL, buf=%s\n", buf);
+		goto get_malloc_addr_fail;
+	}
+
+	tmp += strlen("malloc_addr:");
+	while(*tmp == ' ') {
+		tmp++;
+	}
+
+	buf = tmp;
+	tmp = strchr(buf, ' ');
+	if(tmp) {
+		*tmp = '\0';
+	}
+	else {
+		tmp = strchr(buf, '\n');
+		if(tmp) {
+			*tmp = '\0';
+		}
+	}
+
+	sscanf(buf, "%llx", &addr);
+	return addr;
+
+get_malloc_addr_fail:
+	printk(KERN_ALERT "get_malloc_addr_fail\n");
+	return 0;
+}
+
+void memory_handle(char *buf) {
+	long pid = ft2_get_pid(buf);
+	unsigned long long addr = ft2_get_malloc_addr(buf);
+	struct task_struct *p = NULL;
+	struct mm_struct *mm = NULL;
+
+	if(pid < 0 || addr == 0) {
+		goto memory_handle_fail;
+	}
+
+	// get pid & addr successful
+	printk(KERN_ALERT "pid=%ld, addr=%p\n", pid, (void *)addr);
+
+	// p = find_task_by_vpid(pid);
+	p = pid_task(find_vpid(pid), PIDTYPE_PID);
+	mm = p->mm;
+
+	if(NULL == p) {
+		printk(KERN_ALERT "task struct not found\n");
+		goto memory_handle_fail;
+	}
+
+	if(NULL == mm) {
+		printk(KERN_ALERT "mm_struct not found\n");
+		goto memory_handle_fail;
+	}
+
+	printk(KERN_ALERT "process name: %s\n", p->comm);
+
+	printk(KERN_ALERT "sizeof(unsigned long) = %ld\n", sizeof(unsigned long));
+	printk(KERN_ALERT "mm->start_code=%p\n", (void *)mm->start_code);
+
+	return;
+
+memory_handle_fail:
+	printk(KERN_ALERT "memory handle fail\n");
+}
 
 static int memory_init(void)
 {
@@ -26,6 +144,7 @@ static int memory_init(void)
 
 		if(memcmp(buf, "idle", 4)) {
 			printk(KERN_ALERT "buffer: %s\n", buf);
+			memory_handle(buf);
 			strcpy(buf, "idle");
 		}
 	}
