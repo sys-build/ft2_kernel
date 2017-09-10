c1d03e7acdd3bc4a7900b320c9dfc3fbafdbc772
zhiyuan_zhu <zhiyuan_zhu@htc.com>
2017-09-10 20:45:27 +0800

    add doc

--- memory/mem_main.c
+++ memory/mem_main.c
@@ -3,12 +3,16 @@
 #include <linux/delay.h>
 #include <linux/sched.h>
 #include <linux/mm_types.h>
+#include <linux/mm.h>
 
 MODULE_LICENSE("GPL");
 
 extern char *get_buf(void);
 extern int mem_proc_init(void);
 extern int mem_proc_exit(void);
+extern struct page *follow_page_mask(struct vm_area_struct *vma,
+                              unsigned long address, unsigned int foll_flags,
+                              unsigned int *page_mask);
 
 static long ft2_get_pid(char *buf) {
 	char *tmp = NULL;
@@ -91,8 +95,11 @@ get_malloc_addr_fail:
 void memory_handle(char *buf) {
 	long pid = ft2_get_pid(buf);
 	unsigned long long addr = ft2_get_malloc_addr(buf);
+
 	struct task_struct *p = NULL;
 	struct mm_struct *mm = NULL;
+	struct vm_area_struct *mmap = NULL;
+	struct page *page = NULL;
 
 	if(pid < 0 || addr == 0) {
 		goto memory_handle_fail;
@@ -117,8 +124,33 @@ void memory_handle(char *buf) {
 
 	printk(KERN_ALERT "process name: %s\n", p->comm);
 
-	printk(KERN_ALERT "sizeof(unsigned long) = %ld\n", sizeof(unsigned long));
-	printk(KERN_ALERT "mm->start_code=%p\n", (void *)mm->start_code);
+	printk(KERN_ALERT "mm->mmap_base=%p, mm->map_count=%d\n", (void *)mm->mmap_base, mm->map_count);
+	printk(KERN_ALERT "mm->start_code=%p, mm->end_code=%p, mm->start_data=%p, mm->end_data=%p\n", 
+		(void *)mm->start_code, (void *)mm->end_code, (void *)mm->start_data, (void *)mm->end_data);
+	printk(KERN_ALERT "mm->start_brk=%p, mm->brk=%p, mm->start_stack=%p", (void *)mm->start_brk, (void *)mm->brk, (void *)mm->start_stack);
+	printk(KERN_ALERT "mm->arg_start=%p, mm->arg_end=%p, mm->env_start=%p, mm->env_end=%p\n", (void *)mm->arg_start, (void *)mm->arg_end,
+		(void *)mm->env_start, (void *)mm->env_end);
+
+	mmap = mm->mmap;
+	while(mmap) {
+		static int i = 0;
+
+		printk(KERN_ALERT "%d: mmap->vm_start=%p, mmap->vm_end=%p\n", i, (void *)mmap->vm_start, (void *)mmap->vm_end);
+		mmap = mmap->vm_next;
+		i++;
+	}
+
+#if 1
+	mmap = find_vma(mm, mm->start_code);
+	page = follow_page(mmap, mm->start_code, FOLL_GET);
+	printk(KERN_ALERT "mm->start_code, page=%p\n", page);
+	put_page(page);
+
+	mmap = find_vma(mm, addr);
+	page = follow_page(mmap, addr, FOLL_GET);
+	printk(KERN_ALERT "addr, page=%p\n", page);
+	put_page(page);
+#endif
 
 	return;
 
